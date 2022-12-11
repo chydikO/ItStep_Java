7ad21076dcfdb87e8909362914436cdce8080b63
chydikO_x220 <oleg.chudnovskiy.gmail.com>
2022-12-11 22:59:00 +0200

    Less-03 hw Task-01,02

--- src/com/chudnovskiy/oleg/lesson3/homework/Task01.java
+++ src/com/chudnovskiy/oleg/lesson3/homework/Task01.java
@@ -1,5 +1,7 @@
 package com.chudnovskiy.oleg.lesson3.homework;
 
+import static com.chudnovskiy.oleg.lesson3.homework.Util.scanner;
+
 /**
  * Задание 1
  * Написать программу, которая предлагает пользователю
@@ -8,4 +10,36 @@ package com.chudnovskiy.oleg.lesson3.homework;
  * использованием switch.
  */
 public class Task01 {
+    public static void main(String[] args) {
+        int day;
+
+        System.out.print("Input day of week:\t");
+        day = scanner.nextInt();
+
+        switch (day) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+            default:
+                System.out.println("Error input!");
+        }
+    }
 }
--- src/com/chudnovskiy/oleg/lesson3/homework/Task02.java
+++ src/com/chudnovskiy/oleg/lesson3/homework/Task02.java
@@ -1,5 +1,7 @@
 package com.chudnovskiy.oleg.lesson3.homework;
 
+import static com.chudnovskiy.oleg.lesson3.homework.Util.scanner;
+
 /**
  * Задание 2
  * С клавиатуры вводится время (количество часов от 0 до
@@ -8,4 +10,20 @@ package com.chudnovskiy.oleg.lesson3.homework;
  * приветствие «Добрый день»).
  */
 public class Task02 {
+    public static void main(String[] args) {
+        int hours;
+
+        System.out.print("Enter hours\t");
+        hours = scanner.nextInt();
+
+        if (hours >= 5 && hours <= 11) {
+            System.out.println("Good Morning");
+        } else if (hours >= 12 && hours <= 16) {
+            System.out.println("Good Afternoon");
+        } else if (hours >= 17 && hours <= 20) {
+            System.out.println("Good Evening");
+        } else if (hours >= 21 && hours <= 4) {
+            System.out.println("Good Night");
+        }
+    }
 }
