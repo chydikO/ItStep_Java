94ea715c9da1ebff4b954d6d4be3bd0215a6736a
Chudnovskiy.O.W520 <oleg.chudnovskiy@gmail.com>
2022-12-23 06:18:38 +0200

    add classTask 20,22-12-22

---
+++ src/com/chudnovskiy/oleg/less221222/classwork/LessonApp.java
@@ -0,0 +1,16 @@
+package com.chudnovskiy.oleg.less221222.classwork;
+
+/**
+ * Практика на методи
+ * Due today at 11:59 PM
+ * Instructions
+ * Створити програму, яка дозволяє користувачу розрахувати індекс маси тіла (BMI) і отримати розшифровку результату.
+ *
+ * В програмі повинно бути 2 методи
+ * Перший calculateBodyMassIndex повинен приймати два аргументи - масу тіла в кілограмах і зріст в метарах і повертати ІМТ
+ * Другий printClassification повинен приймати ІМТ і виводити в консоль класифікацію згідно таблиці з вікіпедія
+ * https://uk.wikipedia.org/wiki/%D0%86%D0%BD%D0%B4%D0%B5%D0%BA%D1%81_%D0%BC%D0%B0%D1%81%D0%B8_%D1%82%D1%96%D0%BB%D0%B0?classId=b0417c6e-d157-4403-88dd-97b509741b5d&assignmentId=4bf06512-e8bf-4d5c-89d5-3125ab8afbc1&submissionId=66badc95-6d75-d094-6413-60fc89ca68eb
+ * http://surl.li/edrxd
+ */
+public class LessonApp {
+}
