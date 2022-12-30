ae58443cd8e2900d6952c363c7f2fdacbfe4d7a0
MbPro13_chudikO <oleg.chudnovskiy.gmail.com>
2022-12-30 08:07:04 +0200

    hw 201222 Task03 arrayTransformation & arrayTransformation in progres

--- src/com/chudnovskiy/oleg/less201222/homework/Task03.java
+++ src/com/chudnovskiy/oleg/less201222/homework/Task03.java
@@ -21,33 +21,33 @@ public class Task03 {
         arrayInit();
         arrayFill();
         arrayPrint(array);
-        //arrayPrint(arrayTransformation());
+        arrayPrint(arrayTransformation());
 
     }
 
-   /* private static int[][] arrayTransformation() {
-        int rows = getArrayRows();
-        int[][] newArray = new int[rows][];
+    private static int[] arrayTransformation() {
+        int newArraySize = getNewArraySize();
+        int[] newArray = new int[newArraySize];
         int startIndex = 0;
         int stopIndex;
 
-        for (int i = 0; i < rows; i++) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+
+            }
             newArray[i] = (i + 1 < rows) ? new int[COL] : new int[array.length % COL];
             stopIndex = newArray[i].length;
             newArray[i] = Arrays.copyOfRange(array, startIndex, startIndex + stopIndex);
             startIndex = newArray[i].length;
         }
         return newArray;
-    }*/
+    }
 
-    /*private static int getArrayRows() {
-        int rows = array.length / COL;
-        double realRows = (double) array.length / (double) COL;
-        if (realRows % 2 != 0) {
-            rows++;
-        }
-        return rows;
-    }*/
+    private static int getNewArraySize() {
+        int arraySize = 0;
+
+        return arraySize;
+    }
 
     private static void arrayPrint(int[] array) {
         System.out.println(Arrays.toString(array));
