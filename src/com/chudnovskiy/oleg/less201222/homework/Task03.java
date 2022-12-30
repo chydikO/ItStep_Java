181c8e65547d52c7a0d6f3adeb2dc1c4c6f75150
MbPro13_chudikO <oleg.chudnovskiy.gmail.com>
2022-12-30 07:57:02 +0200

    hw 201222 Task03 arrayInit + arrayFill

--- src/com/chudnovskiy/oleg/less201222/homework/Task03.java
+++ src/com/chudnovskiy/oleg/less201222/homework/Task03.java
@@ -1,5 +1,9 @@
 package com.chudnovskiy.oleg.less201222.homework;
 
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
 /**
  * Завдання 3
  * Є двовимірний масив заповнений випадковими числами. Кількість рядків задається з клавіатури.
@@ -7,4 +11,70 @@ package com.chudnovskiy.oleg.less201222.homework;
  * Перетворити цей масив на одновимірний, що складається з даних двовимірного масиву.
  */
 public class Task03 {
+    private static final Random random = new Random();
+    private static final Scanner scanner = new Scanner(System.in);
+    private static int[][] array;
+    private static int arrayRows;
+
+
+    public static void main(String[] args) {
+        arrayInit();
+        arrayFill();
+        arrayPrint(array);
+        //arrayPrint(arrayTransformation());
+
+    }
+
+   /* private static int[][] arrayTransformation() {
+        int rows = getArrayRows();
+        int[][] newArray = new int[rows][];
+        int startIndex = 0;
+        int stopIndex;
+
+        for (int i = 0; i < rows; i++) {
+            newArray[i] = (i + 1 < rows) ? new int[COL] : new int[array.length % COL];
+            stopIndex = newArray[i].length;
+            newArray[i] = Arrays.copyOfRange(array, startIndex, startIndex + stopIndex);
+            startIndex = newArray[i].length;
+        }
+        return newArray;
+    }*/
+
+    /*private static int getArrayRows() {
+        int rows = array.length / COL;
+        double realRows = (double) array.length / (double) COL;
+        if (realRows % 2 != 0) {
+            rows++;
+        }
+        return rows;
+    }*/
+
+    private static void arrayPrint(int[] array) {
+        System.out.println(Arrays.toString(array));
+    }
+
+    private static void arrayPrint( int[][] array) {
+        for (int i = 0; i < array.length; i++) {
+            arrayPrint(array[i]);
+        }
+    }
+
+    private static void arrayInit() {
+        do {
+            System.out.print("Input array rows 1....100:\t");
+            arrayRows = scanner.nextInt();
+        } while (arrayRows <= 0 || arrayRows >= 100);
+        array = new int[arrayRows][];
+    }
+
+    private static void arrayFill() {
+        int column = 0;
+        for (int i = 0; i < arrayRows; i++) {
+            array[i] = new int[column + 1];
+            for (int j = 0; j < array[i].length; j++) {
+                array[i][j] = random.nextInt(100) + 1;
+            }
+            column++;
+        }
+    }
 }
