b0091619d96eb6e29e0d5ddf703b3759492e20a2
chydikO_x220 <oleg.chudnovskiy.gmail.com>
2023-01-12 15:36:31 +0200

    add less100123 Task02

--- src/com/chudnovskiy/oleg/less10012023/homework/Task02.java
+++ src/com/chudnovskiy/oleg/less10012023/homework/Task02.java
@@ -1,5 +1,8 @@
 package com.chudnovskiy.oleg.less10012023.homework;
+import java.util.Scanner;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 /**
  * Задание 2. Точная степень двойки
  * Дано натуральное число N. Выведите слово YES, если число
@@ -10,4 +13,20 @@ package com.chudnovskiy.oleg.less10012023.homework;
  * Вывод: NO YES
  */
 public class Task02 {
+    public static void main(String[] args) {
+        assertFalse(isPowerOfTwo(3));
+        assertTrue(isPowerOfTwo(8));
+    }
+
+    static boolean isPowerOfTwo(int number) {
+        if(number == 2) {
+            return true;
+        }
+
+        if(number % 2 != 0) {
+            return false;
+        } else {
+            return isPowerOfTwo(number / 2);
+        }
+    }
 }
